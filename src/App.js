072b78b2f8ac7ff9d2734f9678cdc6db9b526067
ojjasvi jain <ojjasvi201@gmail.com>
2021-11-29 17:32:13 +0530

    Calculator on react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Title from "./component/Title"
+import Calculator from "./component/Calculator"
+import "./css/style.css";
+
+
+function App() {
+  return (
+		<div className="container">
+			<Title />
+			<Calculator />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/component/Title.js
@@ -0,0 +1,10 @@
+// Title component
+export default () => {
+    return (
+      <div className="title">
+        <span className="caption">Simple <strong>Calculator</strong>
+        </span>
+      </div>
+    );
+  };
+  
